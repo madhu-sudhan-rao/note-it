8fd64baf1b044bf2ece59da152130b78188685f0
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-04-17 11:58:38 +0530

    added login button to navbar when the user is not logged in

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,12 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
+  const {isAuthenticated, isLoading} = useConvexAuth()
   const scrolled = useScrollTop();
 
   return (
@@ -20,6 +24,30 @@ const Navbar = () => {
         <Logo />
       </div>
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+
+        {
+          isLoading && (
+            <p>Loading...</p>
+          )
+        }
+
+        {
+          !isAuthenticated && !isLoading && (
+            <>
+              <SignInButton mode="modal">
+                <Button variant={"ghost"} size={"sm"}>
+                  Login
+                </Button>
+              </SignInButton>
+              <SignInButton mode="modal">
+                <Button size={"sm"}>
+                  Get Note it free
+                </Button>
+              </SignInButton>
+            </>
+          )
+        }
+        
         <ModeToggle />
       </div>
     </div>

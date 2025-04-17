ee8001908e9fdf314f638299487e8660512a86ec
Madhu Sudhan Rao <madhusudanrao.pediredla@imaginnovate.com>
2025-04-17 12:26:24 +0530

    added user login ability to navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,12 +5,13 @@ import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
-  const {isAuthenticated, isLoading} = useConvexAuth()
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -25,30 +26,30 @@ const Navbar = () => {
         <Logo />
       </div>
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
 
-        {
-          isLoading && (
-            <Spinner />
-          )
-        }
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size={"sm"}>
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size={"sm"}>Get Note it free</Button>
+            </SignInButton>
+          </>
+        )}
 
-        {
-          !isAuthenticated && !isLoading && (
-            <>
-              <SignInButton mode="modal">
-                <Button variant={"ghost"} size={"sm"}>
-                  Login
-                </Button>
-              </SignInButton>
-              <SignInButton mode="modal">
-                <Button size={"sm"}>
-                  Get Note it free
-                </Button>
-              </SignInButton>
-            </>
-          )
-        }
-        
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} size={"sm"} asChild>
+              <Link href="/documents">Start Noting</Link>
+            </Button>
+            <UserButton afterSwitchSessionUrl="/" />
+          </>
+        )}
+
         <ModeToggle />
       </div>
     </div>
